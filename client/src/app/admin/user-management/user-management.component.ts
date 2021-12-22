0547cf9c436685143e1f700b4da863c1d4ff2003
John-Mulkearns <manoflondon@hotmail.com>
2021-12-22 21:31:32 +0000

    Basic Modal Service addes to Admin Roles - v1.0171

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { RolesModalComponent } from 'src/app/modals/roles-modal/roles-modal.component';
 import { User } from 'src/app/_models/user';
 import { AdminService } from 'src/app/_services/admin.service';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styles: [
-  ]
+  styleUrls: ['./user-management.component.css']
 })
 export class UserManagementComponent implements OnInit {
   users: Partial<User[]>;
-  constructor(private adminService: AdminService) { }
+  bsModalRef: BsModalRef;
+  constructor(private adminService: AdminService, private modalService: BsModalService) { }
 
   ngOnInit(): void {
     this.getUsersWithRoles();
@@ -21,5 +23,21 @@ export class UserManagementComponent implements OnInit {
       this.users = users;
     })
   }
+
+openRolesModal(){
+  const initialState = {
+         list: [
+        'Open a modal with component',
+        'Pass your data',
+        'Do something else',
+        '...'
+      ],
+      title: 'Modal with component'
+    }
+this.bsModalRef=this.modalService.show(RolesModalComponent, {initialState})
+this.bsModalRef.content.closeBtnName='Close';
+}
+
+
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -13,6 +13,7 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { TimeagoModule } from 'ngx-timeago';
+import { BsModalService, BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { TimeagoModule } from 'ngx-timeago';
     BsDatepickerModule.forRoot(),
     PaginationModule.forRoot(), 
     ButtonsModule.forRoot(),
-    TimeagoModule.forRoot()
+    TimeagoModule.forRoot(),
+    ModalModule.forRoot()
   ],
   exports: [
     BsDropdownModule,
@@ -40,7 +42,8 @@ import { TimeagoModule } from 'ngx-timeago';
     BsDatepickerModule,
     PaginationModule,
     ButtonsModule,
-    TimeagoModule
+    TimeagoModule,
+    ModalModule
 
   ]
 })
